01547e96ae47d68dcf4a88b1fe7c6a3e1b1bbae2
ShivamChaube9969 <shivamkchaube@gmail.com>
2017-09-01 10:22:35 +0530

    Update Fibonacci.java

--- src/net/shivam/javacodes/Fibonacci.java
+++ src/net/shivam/javacodes/Fibonacci.java
@@ -3,10 +3,7 @@ package net.shivam.javacodes;
 public class Fibonacci {
 	public static void main(String[] args) {
 		long a = 0, b = 1;
-		for (int i = 1; i <= 30; i++) {
-			if ((a + b) > 200) {
-				break;
-			}
+		while ((a + b) < 200) {
 			System.out.print((a + b) + " ");
 			a = a + b;
 			b = a - b;
